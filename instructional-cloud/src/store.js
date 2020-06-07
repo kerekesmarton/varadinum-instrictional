206961a2f5aae538d815621cd6e310ef20e65de2
Marton Kerekes <kerekes.j.marton@gmail.com>
2020-06-07 17:16:57 +0200

    process env

--- instructional-cloud/src/store.js
+++ instructional-cloud/src/store.js
@@ -1,11 +1,14 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-const url = 'mongodb+srv://mkerekes_mongo:hn79wt@cluster0-p1v4h.mongodb.net/';
+const path = 'cluster0-p1v4h.mongodb.net/';
+
+const url = `mongodb+srv://mkerekes_mongo:${process.env.mongo_db_pwd}@${path}`;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, dbName: 'varadinum-instructional' });
 
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log(`Connected to mongo at ${url}`));
+db.once('open', () => console.log(`Connected to mongodb cluster at ${path}`));
